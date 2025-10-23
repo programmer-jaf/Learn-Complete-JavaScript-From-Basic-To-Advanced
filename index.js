b00cc55d07fe1371848dcdb6d3ae97ecd25b7ed3
Programmer-Jaf <programmerjaf@gmail.com>
2025-10-23 20:51:23 +0600

    basic theory of Javascript

---
+++ index.js
@@ -0,0 +1,17 @@
+/*
+What is JavaScript ? ☑️
+
+Ans: JavaScript is a programming language which is used to make web pages interactive and Dynamic
+
+Is JavaScript Support Object Oriented Programming ? ☑️
+
+Ans: Yes, JavaScript supports Object Oriented Programming
+
+When JavaScript was created ? ☑️
+
+Ans: JavaScript was created by Brendan Eich in 1995. JavaScript was originally called LiveScript.
+
+What is the difference between JavaScript and Java ? ☑️
+
+Ans: JavaScript is a programming language which is used to make web pages interactive and Dynamic. Java is a programming language which is used to develop desktop applications.
+*/

43982b4e9f17557e5e764a520dea2d3d8926fc93
Programmer-Jaf <programmerjaf@gmail.com>
2025-10-23 20:55:40 +0600

    Add basic concepts and examples of JavaScript variables

--- index.js
+++ index.js
@@ -15,3 +15,40 @@ What is the difference between JavaScript and Java ? ☑️
 
 Ans: JavaScript is a programming language which is used to make web pages interactive and Dynamic. Java is a programming language which is used to develop desktop applications.
 */
+
+
+/*
+Basic Concept of JavaScript 👇👇👇
+0. print in console
+1. Variable
+2. Data Types
+3. Operators
+4. Control Flow
+5. Functions
+6. Arrays
+7. Objects
+8. DOM Manipulation
+*/
+
+// print in console
+console.log("👋 JavaScript");
+
+
+/*
+
+Variable 👇👇👇
+
+What is a variable ? ☑️
+
+Ans: A variable is a container for storing data values.If i want to store milk i have to store it into a milk-bottle 🍼
+
+
+*/
+// How to declare a variable ? ☑️
+var milk = "milk";
+
+
+// How to print a variable ? ☑️
+console.log(milk);
+
+
